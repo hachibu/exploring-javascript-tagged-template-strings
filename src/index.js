461eaf9518e102c98d2335f12e4a4d912312f910
hachibu <raymondsohn@gmail.com>
2020-03-03 14:38:08 -0800

    Add squish example

--- src/index.js
+++ src/index.js
@@ -1,24 +1,65 @@
 var { log } = require('./util');
-var { ast, html, md, sexp } = require('./tags');
+var { ast, html, md, sexp, squish } = require('./tags');
+
+// SPLIT EXAMPLES INTO FILES SO EACH EXAMPLE CAN BE SHOWN ON A SLIDE
+// Categories
+//
+// String Manipulation
+// - Squish
+//
+// Language
+// - AST Inspection
+// - Code Generation: Quasiquotes
+// - https://babeljs.io/docs/en/7.8.0/babel-template
+//
+// DSL
+// - GraphQL Queries
+// - Markdown
+// - HTML
+// - SASS
+// - JSX
+// - dom
+// - Embedded Languages (e.g. Lisp)
+//
+// Party Tricks
+// - Promisable lazy template strings
 
 // rpn, scheme, gql
-
+// and list
+// human readable type stuff
 // Create 1 compelling example for each
+// dom tag
 
-// ast
-// Have you ever scratched your head wondering how the association of an
-// expression would be. Like I know * is stronger than + because I was told
-// so but with the ast tag you can demystify what's going on. This could be
-// used as tool to explore and deepen your understanding of JS.
+// Squish Tag
+// Stealing an idea from Ruby on Rails can squish all the whitespace down to one
+// space and trim.
+log(
+  squish`
+    1
+    2
+    3
+  `
+);
+
+// AST Tag
+// Understand JS better by exploring the AST. For example, expression binding.
 //
 // log(ast`1 * 2 + 3`);
 // log(ast`1 + 2 * 3`);
-
 // log(sexp`(+ 1 2)`)
 
-// md
+// Markdown Tag
 // Markdown has only gotten more popular
-// log(md`[Wikipedia](//wikipedia.org)`);
+
+log(
+  md`
+# List
+
+- 1
+- 2
+- 3
+  `
+);
 
 // html
 // log(html`<ul><li>a</li></ul>`);
--- src/tags.js
+++ src/tags.js
@@ -1,14 +1,25 @@
-var createTag = require('./create-tag');
+var { createTag, isFunction } = require('./util');
 
 var parsers = {
-  ast: require('@babel/template').default.ast,
-  html: require('cheerio'),
-  md: require('marked').parse,
-  sexp: require('paredit.js').reader.readSexp,
+  ast: [
+    require('@babel/template').default.ast
+  ],
+  squish: [
+    (result) => result.replace(/[\n\r\s]+/g, ' ').trim()
+  ],
+  md: [
+    (result) => require('marked').parse(result.trim())
+  ],
+  html: [
+    require('cheerio')
+  ],
+  sexp: [
+    require('paredit.js').reader.readSexp
+  ],
 };
 
 module.exports = {};
 
 for (let [k, v] of Object.entries(parsers)) {
-  module.exports[k] = createTag(v);
+  module.exports[k] = createTag(...v);
 }
